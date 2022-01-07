c20a63e44893b5dbb50de4c520aea42304324547
Nicolas peluso <pelusonicolas7@gmail.com>
2022-01-07 06:43:43 -0300

    Editar usuario

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post("/user/register", (req, res) => {
         let Senha = req.body.Senha
         //verificando o corpo da requisisao
         if (req.body == {} || Nome === "" || Email === "" || Senha === "") throw new Error("Email ou Nome Vazio")
-        if (typeof Nome !== "string" || typeof Email !== "string") throw new Error("Email ou Nome devem ser texto")
+        if (typeof Nome !== "string" || typeof Email !== "string") throw new Error("Email ou Nome devem ser string")
         //Inserindo No arquivo json 
         userRegister.users.push({ Nome, Email, Senha: MD5Encripter(Senha) })
         WriteingInFile()
@@ -50,6 +50,38 @@ app.post("/user/register", (req, res) => {
     }
 })
 //Editar usuario
+
+app.put("/user/Edit", (req, res) => {
+    try {
+        const Email = req.body.Email
+        const Senha = req.body.Senha
+        let NewSenha = req.body.NovaSenha
+        const NewEmail = req.body.NovoEmail
+
+        if (Email === "" || Senha === "" || NewSenha === "" || NewEmail === "") throw new Error("Prencha todos os campos")
+        if (typeof Email !== "string" || typeof NewEmail !== "string") throw new Error("Email e/ou novo Emaild deven ser string")
+        //Verificar se o usuario existe No banco
+        let HasUser = userRegister.users.reduce((acc, cur, index) => {
+            cur.Email == Email ? acc.Email = cur.Email : null
+            MD5Encripter(Senha) == cur.Senha ? acc.Senha = MD5Encripter(cur.Senha) : null
+            acc.Index = index
+            return acc
+        }, { Email: null, Senha: null, newSenha: MD5Encripter(NewSenha), newEmail: NewEmail })
+        //se o usuario nao bater com algum do banco
+        if (HasUser.Email == null) throw new Error("Email invalido")
+        if (HasUser.Senha == null) throw new Error("senha invalida")
+        //Alterando usuario no banco
+        let keys = ["Email", "Senha"]
+        keys.forEach(key => userRegister.users[HasUser.Index][key] = HasUser["new" + [key]])
+        WriteingInFile()
+
+        return res.json({ "message": "Usuario Editado com sucesso" })
+
+    }
+    catch (err) {
+        res.json({ "message": `${err}` })
+    }
+})
 
 
 app.listen(Port, () => {
